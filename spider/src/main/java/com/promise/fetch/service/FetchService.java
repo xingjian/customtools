9b7800f1356538b0f13c9ec2b21025f692a5f7dc
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2011-09-13 01:56:54 +0000

    git-svn-id: svn://10.10.191.1:3690/customtools/trunk@235 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ spider/src/main/java/com/promise/fetch/service/FetchService.java
@@ -0,0 +1,14 @@
+/*@文件名: FetchService.java  @创建人: 邢健   @创建日期: 2011-8-8 下午12:00:35*/
+package com.promise.fetch.service;
+
+/**   
+ * @类名: FetchService.java 
+ * @包名: com.promise.fetch.service 
+ * @描述: 抓取服务接口 
+ * @作者: 邢健 xingjian@yeah.net   
+ * @日期: 2011-8-8 下午12:00:35 
+ * @版本 V1.0   
+ */
+public interface FetchService {
+
+}
---
+++ spider/src/main/java/com/promise/fetch/service/impl/FetchServiceImpl.java
@@ -0,0 +1,16 @@
+/*@文件名: FetchServiceImpl.java  @创建人: 邢健   @创建日期: 2011-8-8 下午12:01:17*/
+package com.promise.fetch.service.impl;
+
+import com.promise.fetch.service.FetchService;
+
+/**   
+ * @类名: FetchServiceImpl.java 
+ * @包名: com.promise.fetch.service.impl 
+ * @描述: 抓取服务接口实现类 
+ * @作者: 邢健 xingjian@yeah.net   
+ * @日期: 2011-8-8 下午12:01:17 
+ * @版本 V1.0   
+ */
+public class FetchServiceImpl implements FetchService {
+
+}
